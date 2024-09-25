8dce54ef472fc78f501760101af8bdedf2e6243d
A-chan <mtureit@gmail.com>
2024-09-25 14:55:37 +0900

    [feat] createResponsive

--- front/app/src/components/InputBox/index.tsx
+++ front/app/src/components/InputBox/index.tsx
@@ -24,13 +24,27 @@ export default function Home() {
   useEffect(() => {
     const textElement = textTopRef.current;
     if (textElement) {
-      const maxFontSize = 24;
-      const minFontSize = 16;
+      // 画面幅に応じてフォントサイズの範囲を決定
+      let maxFontSize, minFontSize;
+      const windowWidth = window.innerWidth;
+
+      if (windowWidth <= 600) {
+        // スマホサイズ
+        maxFontSize = 16;
+        minFontSize = 8;
+      } else {
+        // デスクトップサイズ
+        maxFontSize = 24; // デスクトップの場合の最大フォントサイズ
+        minFontSize = 16;
+      }
+
       let fontSize = maxFontSize;
       const containerWidth = textElement.offsetWidth;
 
+      // 初期フォントサイズを設定
       textElement.style.fontSize = `${fontSize}px`;
 
+      // テキストが要素に収まるようにフォントサイズを調整
       while (
         textElement.scrollWidth > containerWidth &&
         fontSize > minFontSize
@@ -38,7 +52,8 @@ export default function Home() {
         fontSize -= 1;
         textElement.style.fontSize = `${fontSize}px`;
 
-        const topValue = 25 - (maxFontSize - fontSize) / 12;
+        // フォントサイズに応じて `top` の値を調整
+        const topValue = 25 - (maxFontSize - fontSize) / 8;
         textElement.style.top = `${topValue}%`;
       }
     }
@@ -56,7 +71,7 @@ export default function Home() {
         setTextColor("#529B30");
         break;
       case "渉外局":
-        setBackgroundImage("/images/shougai.png");
+        setBackgroundImage("/images/syougai.png");
         setTextColor("#2C7184");
         break;
       case "情報局":
@@ -94,7 +109,19 @@ export default function Home() {
     }
 
     setLoading(true);
-    toPng(cardRef.current)
+
+    // 要素の現在のサイズを取得
+    const cardWidth = cardRef.current.offsetWidth;
+    const cardHeight = cardRef.current.offsetHeight;
+
+    // 画面の幅に応じて解像度の倍率を決定
+    const pixelRatio = window.innerWidth <= 600 ? 8 : 4; // 600px以下なら2倍、それ以上は4倍
+
+    toPng(cardRef.current, {
+      width: cardWidth,
+      height: cardHeight,
+      pixelRatio: pixelRatio, // 解像度の倍率を設定
+    })
       .then((dataUrl: string) => {
         download(dataUrl, "profile-card.png");
       })
@@ -129,46 +156,52 @@ export default function Home() {
         </div>
       </div>
       <div className={s.component}>
-        <form>
-          <div className={s.inputForm}>
-            <label>名前・ニックネーム(無い場合は省略)</label>
-            <input
-              type="text"
-              name="name"
-              placeholder="例)長岡　太郎(おかたろ)"
-              value={profile.name}
-              onChange={handleChange}
-            />
-          </div>
-          <div className={s.inputForm}>
-            <label>所属局</label>
-            <select
-              name="office"
-              value={profile.office}
-              onChange={handleChange}
-            >
-              <option value="">-------</option>
-              <option value="企画局">企画局</option>
-              <option value="財務局">財務局</option>
-              <option value="渉外局">渉外局</option>
-              <option value="情報局">情報局</option>
-              <option value="制作局">制作局</option>
-              <option value="総務局">総務局</option>
-            </select>
-          </div>
-          <div className={s.inputForm}>
-            <label>学年</label>
-            <select name="grade" value={profile.grade} onChange={handleChange}>
-              <option value="">-------</option>
-              <option value="B1">B1</option>
-              <option value="B2">B2</option>
-              <option value="B3">B3</option>
-              <option value="B4">B4</option>
-              <option value="M1">M1</option>
-              <option value="M2">M2</option>
-            </select>
-          </div>
-        </form>
+        <div className={s.inputForms}>
+          <form>
+            <div className={s.inputForm}>
+              <label>名前・ニックネーム(無い場合は省略)</label>
+              <input
+                type="text"
+                name="name"
+                placeholder="例)長岡　太郎(おかたろ)"
+                value={profile.name}
+                onChange={handleChange}
+              />
+            </div>
+            <div className={s.inputForm}>
+              <label>所属局</label>
+              <select
+                name="office"
+                value={profile.office}
+                onChange={handleChange}
+              >
+                <option value="">-------</option>
+                <option value="企画局">企画局</option>
+                <option value="財務局">財務局</option>
+                <option value="渉外局">渉外局</option>
+                <option value="情報局">情報局</option>
+                <option value="制作局">制作局</option>
+                <option value="総務局">総務局</option>
+              </select>
+            </div>
+            <div className={s.inputForm}>
+              <label>学年</label>
+              <select
+                name="grade"
+                value={profile.grade}
+                onChange={handleChange}
+              >
+                <option value="">-------</option>
+                <option value="B1">B1</option>
+                <option value="B2">B2</option>
+                <option value="B3">B3</option>
+                <option value="B4">B4</option>
+                <option value="M1">M1</option>
+                <option value="M2">M2</option>
+              </select>
+            </div>
+          </form>
+        </div>
       </div>
       <div className={s.component}>
         <button
