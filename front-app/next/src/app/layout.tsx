aec0c91be71cc775e23600e49b4709685f683893
A-chan <mtureit@gmail.com>
2024-09-25 21:26:45 +0900

    [fix] createEnvironment

---
+++ front-app/next/src/app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from "next";
+import { Kosugi_Maru } from "next/font/google";
+import "./globals.css";
+
+const kosugiMaru = Kosugi_Maru({
+  weight: "400",
+  subsets: ["latin"],
+  display: "swap",
+});
+
+export const metadata: Metadata = {
+  title: "electronic-business-card",
+  description: "電子名刺のデザイン",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="ja">
+      <head></head>
+      <body className={kosugiMaru.className}>{children}</body>
+    </html>
+  );
+}
